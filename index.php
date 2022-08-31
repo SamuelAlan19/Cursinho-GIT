8403a2538cdce1c8367fdaf30873772ecfd1e05c
Samuel <samuelsantos@sescmg.com.br>
2022-08-31 15:50:11 -0300

    Alteração para PHP

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php 
-echo phpinfo();
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto para Front-End</title>
+    <link rel="stylesheet" href="css/style.css"> 
+</head>
+<body>
+    <script src="js/script.js"></script>
+</body>
+</html>
 

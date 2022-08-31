bbe00da1d8518a9378d96611ec8e11c784b6493a
Samuel <samuelsantos@sescmg.com.br>
2022-08-31 15:57:14 -0300

    UPD data-hora

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto para Front-End</title>
+    <title>Projeto para Front-End <?php
+    echo date('Y-m-d');
+    ?></title>
     <link rel="stylesheet" href="css/style.css"> 
 </head>
 <body>
+    <h1> Fucionando! </h1>
+
+    
     <script src="js/script.js"></script>
 </body>
 </html>

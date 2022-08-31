d5b3882a99d86d715d8f3e12d6fce77b76b9cad6
Samuel <samuelsantos@sescmg.com.br>
2022-08-31 16:53:19 -0300

    Progressbar

--- index.php
+++ index.php
@@ -5,13 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Front-End <?php echo date('Y-m-d');?> </title>
+    
     <!-- Bootstrap -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css"> 
 
     <!-- Script -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    
+    <!-- Font Awesome -->
+
+    <script src="https://kit.fontawesome.com/22bd473149.js" crossorigin="anonymous"></script>
+
+    <!-- Progress Bar-->
+
+    <script src = "js/progressbar.min.js"></script>
+
+    <!--Parallax-->
+
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+
 </head>
 <body>
     <h1> Fucionando! </h1>

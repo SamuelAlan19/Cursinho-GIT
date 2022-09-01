f978619484041b6e5c9cc5038975cc62f05da47e
Samuel <samuelsantos@sescmg.com.br>
2022-09-01 13:59:02 -0300

    UPD Erro no button

--- index.php
+++ index.php
@@ -7,42 +7,33 @@
     <title>Projeto Front-End <?php echo date('Y-m-d');?> </title>
     
     <!-- Bootstrap -->
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css"> 
-
     <!-- Script -->
-
     <script
     src="https://code.jquery.com/jquery-3.6.1.min.js"
     integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
     crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>   
     <!-- Font Awesome -->
-
     <script src="https://kit.fontawesome.com/22bd473149.js" crossorigin="anonymous"></script>
-
-    <!-- Progress Bar-->
-
+   <!-- Progress Bar-->
     <script src="js/progressbar.min.js"></script>
-
     <!--Parallax-->
-
     <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 
 </head>
 <body>
     <header>
-        <div class="conteiner" id="nav-conteiner">
-            <nav class="navbar navbr-expand-lg fixed-top">
+        <div class="container" id="nav-container">
+            <nav class="navbar navbar-expand-lg fixed-top">
                     <a href="#" class="navbar-brand">
-                    <img src="img/hdcagency_logo.svg" alt="hDC Agency"> Samuel's Agency
+                        <img src="img/hdcagency_logo.svg" alt="hDC Agency"> Samuel's Agency
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" 
-                        aria-controls="navbar-links" aria-expanded="true" aria-label="Toggle navigation"> 
-                    <span class="navbar-toggler-icon"></span>
+                        aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation"> 
+                        <span class="navbar-toggler-icon"></span>
                     </button> 
                 <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
                     <div class="navbar-nav">
@@ -59,7 +50,8 @@
     </header>
 
 
-    <nav class="navbar navbar-expand-lg bg-light">
+
+ <!--   <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>

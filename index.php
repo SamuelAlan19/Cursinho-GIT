c6bd4b98a91b86f0f09aa44a3793cdd21962b851
Samuel <samuelsantos@sescmg.com.br>
2022-09-01 13:38:03 -0300

    Alterações no Script

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 
     <!-- Script -->
 
+    <script
+    src="https://code.jquery.com/jquery-3.6.1.min.js"
+    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+    crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     
@@ -22,7 +26,7 @@
 
     <!-- Progress Bar-->
 
-    <script src = "js/progressbar.min.js"></script>
+    <script src="js/progressbar.min.js"></script>
 
     <!--Parallax-->
 
@@ -30,13 +34,57 @@
 
 </head>
 <body>
-    <div class = "conteiner id="nav-conteiner">
-        <nav class = "navbar navbr-expand-lg fixed-top">
-            <a href = "#" class = "navbar-brand">
-                <img src = "img/hdcagency_logo.svg" alt = "hDC Agency"> Samuel Agency
-            </a>
-        </nav>
+    <header>
+        <div class="conteiner" id="nav-conteiner">
+            <nav class="navbar navbr-expand-lg fixed-top">
+                    <a href="#" class="navbar-brand">
+                    <img src="img/hdcagency_logo.svg" alt="hDC Agency"> Samuel's Agency
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" 
+                        aria-controls="navbar-links" aria-expanded="true" aria-label="Toggle navigation"> 
+                    <span class="navbar-toggler-icon"></span>
+                    </button> 
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav">
+                        <a href="nav-item nav-link" id="home-menu">Home</a>
+                        <a href="nav-item nav-link" id="about-menu">A agência</a>
+                        <a href="nav-item nav-link" id="services-menu">Serviços</a>
+                        <a href="nav-item nav-link" id="team-menu">Time</a>
+                        <a href="nav-item nav-link" id="portifolio-menu">Projetos</a>
+                        <a href="nav-item nav-link" id="contact-menu">Contato</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">FUNCIONOU</a>
+    <a href="nav-item nav-link" id="home-menu">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     <h1> Working! </h1>
     <script src="js/script.js"></script>
 </body>

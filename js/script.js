6c6e9075f731a44e08498eced30cdd5b8bdfa4b3
Samuel <samuelsantos@sescmg.com.br>
2022-09-05 12:32:29 -0300

    Error Animação das Telas

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let containerA = document.getElementById("circleA")
 let circleA = new ProgressBar.Circle(containerA, {
 
 color: '#64DAF9',
-stroke: 8,
+strokeWidth: 8,
 duration: 1400,
 from: {color: '#AAA'},
 to: {color: '#64DAF9'},
@@ -23,7 +23,89 @@ step: function(state, circle) {
 
 });
 
-circleA.animate(1.0);
+let containerB = document.getElementById("circleB")
 
-}); 
+let circleB = new ProgressBar.Circle(containerB, {
 
+color: '#64DAF9',
+strokeWidth: 8,
+duration: 1600,
+from: {color: '#AAA'},
+to: {color: '#64DAF9'},
+
+step: function(state, circle) {
+
+    circle.path.setAttribute('stroke', state.color);
+
+    let value = Math.round(circle.value() * 254);
+
+    circle.setText(value);
+
+}
+
+});
+
+let containerC = document.getElementById("circleC")
+
+let circleC = new ProgressBar.Circle(containerC, {
+
+color: '#64DAF9',
+strokeWidth: 8,
+duration: 2000,
+from: {color: '#AAA'},
+to: {color: '#64DAF9'},
+
+step: function(state, circle) {
+
+    circle.path.setAttribute('stroke', state.color);
+
+    let value = Math.round(circle.value() * 32);
+
+    circle.setText(value);
+
+}
+
+});
+
+let containerD = document.getElementById("circleD")
+
+let circleD = new ProgressBar.Circle(containerD, {
+
+color: '#64DAF9',
+strokeWidth: 8,
+duration: 2200,
+from: {color: '#AAA'},
+to: {color: '#64DAF9'},
+
+step: function(state, circle) {
+
+    circle.path.setAttribute('stroke', state.color);
+
+    let value = Math.round(circle.value() * 300);
+
+    circle.setText(value);
+}
+
+});
+
+    let dataAreaOffset = $('#data-area').offset();
+    let stop = 0; 
+
+    $(window).scroll(function(e) {
+
+        let scroll = $(window).scrolltop();
+
+        if(scroll > (dataAreaOffset.top -500) && stop == 0){
+
+         circleA.animate(1.0)       
+         circleB.animate(1.0) 
+         circleC.animate(1.0) 
+         circleD.animate(1.0)  
+         
+         stop = 1;
+
+        }
+    }); 
+
+
+    });

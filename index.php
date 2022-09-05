beeaa9ca8fa00f34c188b035d24e89c9996ab0c1
Samuel <samuelsantos@sescmg.com.br>
2022-09-05 13:45:01 -0300

    UPD Parallax + Cards

--- index.php
+++ index.php
@@ -174,6 +174,51 @@
                         </div>
                    </div>
                 </div>
+            </div>
+            <div id="team-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="main-title">Nosso time</h3>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="img/profile1.jpg" class="card-img-top" alt="Imagem de Perfil 1">
+                                <div class="card-body">
+                                    <h5 class="card-title">Fabrício</h5>
+                                    <p class="card-text">Software Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="img/profile2.jpg" class="card-img-top" alt="Imagem de Perfil 2">
+                                <div class="card-body">
+                                    <h5 class="card-title">Juliana</h5>
+                                    <p class="card-text">SEO Consultant</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="img/profile3.jpg" class="card-img-top" alt="Imagem de Perfil 3">
+                                <div class="card-body">
+                                    <h5 class="card-title">Carlos</h5>
+                                    <p class="card-text">Project Manager</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3"> 
+                            <div class="card">
+                                <img src="img/profile4.jpg" class="card-img-top" alt="Imagem de Perfil 4">
+                                <div class="card-body">
+                                    <h5 class="card-title">Patrícia</h5>
+                                    <p class="card-text">UX/UI Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>              
         </div> 
     </main>
